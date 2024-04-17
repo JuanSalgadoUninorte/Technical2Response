28c3eef2f4cb569b7b9a75632963fada03ee2039
Juan David Salgado <jdsa1822@gmail.com>
2024-04-16 21:32:41 -0500

    Visuales

---
+++ app/src/main/java/com/moviles/technical2response/TipsCalculator.kt
@@ -0,0 +1,12 @@
+package com.moviles.technical2response
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TipsCalculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tips_calculator)
+    }
+
+}
